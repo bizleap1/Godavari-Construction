ed21d5166519691fa4e9d9a11dbf7dd49974707e
Indrajit128 <indrajitkshirsagar128@gmail.com>
2026-03-17 14:28:03 +0530

    fix deployment paths and add favicon

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        services: resolve(__dirname, 'services.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      },
+    },
+  },
+});
